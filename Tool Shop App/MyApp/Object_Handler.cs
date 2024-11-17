a02dc84519662b44c7a7a56dd175dcc9381f3baf
Talha Aslam <144408282+mtalha1501@users.noreply.github.com>
2024-11-17 20:05:13 +0500

    Add files via upload

---
+++ Tool Shop App/MyApp/Object_Handler.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ToolShop_DLL;
+using ToolShop_DLL.BL;
+using ToolShop_DLL.DL;
+using ToolShop_DLL.DL.DB;
+using ToolShop_DLL.Interfaces;
+
+namespace MyApp
+{
+    public class Object_Handler
+    {
+        private static I_User user = UserDB.GetInstance();
+        private static I_Manager manager = ManagerDB.GetInstance();
+        private static I_Tool tool = ToolDB.GetInstance();
+       
+
+        public static I_User Get_User() 
+        {
+            return user;
+        }
+        public static I_Manager Get_Manager()
+        {
+            return manager;
+        }
+        public static I_Tool Get_Tool()
+        {
+            return tool;
+        }
+    }
+}
